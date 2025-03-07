31dba30f5fb2d08cefe3f81959e016c7ec68ca4e
infinit3i <matthewiver@protonmail.com>
2025-03-06 22:19:16 -0500

    add multi select option

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,33 @@ mod suricata;
 mod sigma;
 mod splunk;
 
-use dialoguer::Select;
+use dialoguer::MultiSelect;
 
 fn main() {
-    let options = vec!["Yara", "Suricata","Sigma","Splunk"];
-    let selection = Select::new()
-        .with_prompt("Select a tool")
+    let options = vec!["Yara", "Suricata", "Sigma", "Splunk", "All"];
+    
+    let selections = MultiSelect::new()
+        .with_prompt("Select one or more tools (use space to select, enter to confirm):")
         .items(&options)
-        .default(0)
         .interact()
         .unwrap();
 
-    match options[selection] {
-        "Yara" => yara::process_yara(),
-        "Suricata" => suricata::process_suricata(),
-        "Sigma" => sigma::process_sigma(),
-        "Splunk" => splunk::process_splunk(),
-        _ => println!("Invalid selection"),
+    // If "All" is selected (last option) then run all modules.
+    if selections.contains(&(options.len() - 1)) {
+        println!("Processing all tools...");
+        yara::process_yara();
+        suricata::process_suricata();
+        sigma::process_sigma();
+        splunk::process_splunk();
+    } else {
+        for i in selections {
+            match options[i] {
+                "Yara" => yara::process_yara(),
+                "Suricata" => suricata::process_suricata(),
+                "Sigma" => sigma::process_sigma(),
+                "Splunk" => splunk::process_splunk(),
+                _ => println!("Invalid selection"),
+            }
+        }
     }
 }
